4a5fae4615c7411b90866db1a0ebd13cac030d9d
Max Bartlett <maxwellmbartlett@gmail.com>
2020-12-11 10:22:11 -0700

    primitive_multiply

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
 
-
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    def add(x: int, y: int) -> int:
+        while y:
+            carry = x & y
+            x = x ^ y
+            y = carry << 1
+        return x
+    result = 0
+    while x:
+        if x & 1:
+            result = add(result, y)
+        x, y = x >> 1, y << 1
+    return result
 
 
 if __name__ == '__main__':
