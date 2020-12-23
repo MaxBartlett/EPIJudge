950b41bcc1d9792e2c7f29ae153ce97e00c7bc6a
Max Bartlett <maxwellmbartlett@gmail.com>
2020-12-22 21:04:00 -0700

    sorted lists merge

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,16 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    result = cur = ListNode()
+    
+    while L1 and L2:
+        if L1.data < L2.data:
+            cur.next, L1 = L1, L1.next
+        else:
+            cur.next, L2 = L2, L2.next
+        cur = cur.next
+    cur.next = L1 or L2
+    return result.next
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -698,7 +698,7 @@ problem_mapping = {
                 "total": 501
             },
             "Python: sorted_lists_merge.py": {
-                "passed": 0,
+                "passed": 501,
                 "total": 501
             }
         },
