e7e27f63a05b9b1a3a6575e561ce22a31652a86a
Max Bartlett <maxwellmbartlett@gmail.com>
2021-01-05 21:42:45 -0700

    is anonymous letter constructible

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,10 +1,37 @@
 from test_framework import generic_test
+from collections import Counter
 
+def counter(letter_text: str, magazine_text: str) -> bool:
+    magazine_counter = Counter(list(magazine_text))
+    letter_counter = Counter(list(letter_text))
+    return not letter_counter - magazine_counter
+
+def magazine_counter(letter_text: str, magazine_text: str) -> bool:
+    magazine_counter = Counter(list(magazine_text))
+    for c in letter_text:
+        if c not in magazine_counter:
+            return False
+        magazine_counter[c] -= 1
+        if magazine_counter[c] < 0:
+            return False
+    return True
+
+def letter_counter(letter_text: str, magazine_text: str) -> bool:
+    letter_counter = Counter(list(letter_text))
+    for c in magazine_text:
+        if c in letter_counter:
+            letter_counter[c] -= 1
+            if letter_counter[c] == 0:
+                del letter_counter[c]
+                if not letter_counter:
+                    return True
+    return not letter_counter
+    
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    return counter(letter_text, magazine_text)
+    
 
 
 if __name__ == '__main__':
